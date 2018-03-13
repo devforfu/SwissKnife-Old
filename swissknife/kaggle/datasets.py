c4f3a57cede8d173786bc1fea6d500e0f74b95c3
devforfu <developer.z@outlook.com>
2018-03-13 20:04:16 +0500

    Moving code to different file and default image loader

---
+++ swissknife/kaggle/datasets.py
@@ -0,0 +1,267 @@
+"""
+Datasets loading and processing.
+"""
+import csv
+from pathlib import Path
+
+import numpy as np
+from sklearn.utils import column_or_1d
+from sklearn.preprocessing import LabelBinarizer
+
+from ..files import FilesStream
+
+
+class KaggleClassifiedImagesSource:
+    """Class that helps generate (x, y) samples from images data provided in
+    Kaggle-specific format.
+
+    Usually each training dataset's image has a unique filename that could be
+    one-to-one mapped onto class that is represented on that image. And, the
+    mapping from filename to class name is stored within separate CSV file.
+
+    For example, consider the following folder with images:
+
+        \dataset
+            \train
+              - 08ab870.jpeg
+              - 73c913f.jpeg
+              - fe10512.jpeg
+              ...
+              - cca89cf.jpeg
+            \test
+              ...
+
+    Then, file with labels should have the following structure:
+
+        +---------+-------+
+        | UUID    | Class |
+        +---------+-------+
+        | 08ab870 | dog   |
+        | 73c913f | cat   |
+        | fe1051  | dog   |
+            ...     ...
+        | cca89cf | fox   |
+        +---------+-------+
+
+    Attributes:
+        labels_path: Path to file with mapping from file names to classes.
+        binarizer: Instance of sklearn.LabelBinarizer class used to convert
+            string classes representation into one-hot encoded vectors.
+        name_to_label: Mapping from string representation of class into its
+            numerical value.
+        identifier_to_label: Mapping from unique filename identifier into
+            its class's numerical value.
+
+    """
+    def __init__(self,
+                 labels_path: str,
+                 label_column: str,
+                 load_image=None):
+
+        if load_image is None:
+            load_image = FallbackImageLoader()
+
+        self.labels_path = labels_path
+        self.label_column = label_column
+        self.binarizer = None
+        self.name_to_label = None
+        self.identifier_to_label = None
+        classes = self.read_labels(labels_path, label_column=label_column)
+        self.build(classes)
+        self._load_image = load_image
+
+    def __call__(self, folder: str, target_size: tuple, batch_size: int=32):
+        return self.flow(folder, target_size, batch_size)
+
+    @property
+    def n_classes(self) -> int:
+        """Returns total number of classes represented by dataset images."""
+        return len(self.binarizer.classes_)
+
+    @staticmethod
+    def read_labels(filename: str, label_column: str, id_column: str='id'):
+        """Reads file with class labels.
+
+        Args:
+            filename: Path to file with labels.
+            id_column: Column with unique image identifier.
+            label_column: Column with verbose classes names.
+
+        Returns:
+            labels: The mapping from ID to verbose label.
+
+        """
+        if not Path(filename).exists():
+            raise ValueError('labels file is not found: %s' % filename)
+        with open(filename) as fp:
+            reader = csv.DictReader(fp)
+            try:
+                labels = {row[id_column]: row[label_column] for row in reader}
+            except KeyError:
+                raise ValueError(
+                    "please check your CSV file: '%s' and/or '%s' "
+                    "column was not found" % (id_column, label_column))
+            else:
+                return labels
+
+    def build(self, classes: dict):
+        """Fits labels one-hot encoder and maps verbose image classes labels
+        onto one-hot encoded vectors.
+
+        Args:
+            classes: A dictionary with classes. Should be a mapping from file
+                ID to string with class name.
+
+        """
+        identifiers = [Path(path).stem for path in classes.keys()]
+        string_labels = list(classes.values())
+        binarizer = LabelBinarizer()
+        one_hot = binarizer.fit_transform(string_labels)
+
+        name_to_label = {
+            binarizer.classes_[vec.argmax()]: vec for vec in one_hot}
+
+        identifier_to_label = {
+            uid: name_to_label[classes[uid]] for uid in identifiers}
+
+        self.binarizer = binarizer
+        self.name_to_label = name_to_label
+        self.identifier_to_label = identifier_to_label
+
+    def get_class_name(self, image_path) -> str:
+        """Returns class represented by image using file name as unique ID."""
+
+        uid = Path(image_path).stem
+        if uid not in self.identifier_to_label:
+            raise ValueError('The file with ID \'%s\' is not present in '
+                             'mapping. Probably it was taken from different '
+                             'dataset or file with labels which was used to '
+                             'build mapping is incomplete.')
+        return self.identifier_to_label[uid]
+
+    def one_hot_to_verbose(self, vec) -> str:
+        """Converts one-hot encoded vector into verbose class name."""
+
+        try:
+            vec = column_or_1d(vec, warn=False)
+        except ValueError:
+            raise ValueError('Input value should be a 1D array or column, '
+                             'but has shape %s instead' % vec.shape)
+
+        if len(vec) > self.n_classes:
+            raise ValueError('Input array is longer then number of classes '
+                             'in dataset: %d > %d' %
+                             (len(vec), self.n_classes))
+
+        return self.binarizer.classes_[vec.argmax()]
+
+    def integer_to_verbose(self, label: int) -> str:
+        """Converts class represented with integer label into verbose class
+        name.
+        """
+        return self.binarizer.classes_[label]
+
+    def flow(self, folder: str, target_size: tuple, batch_size: int):
+        """Creates a generator that infinitely iterates through directory with
+        files and reads images from folder in batches, converting them
+        into (x, y) pairs for model's training.
+
+        Args:
+            folder: Directory with labelled files.
+            target_size: Shape of generated image samples.
+            batch_size: Size of batch.
+
+        """
+        stream = FilesStream(folder, batch_size=batch_size)
+        for paths in stream(infinite=True, same_size_batches=True):
+            arrays = np.asarray([
+                self._load_image(path, target_size=target_size)
+                for path in paths])
+            targets = np.asarray([self.get_class_name(path) for path in paths])
+            yield arrays, targets
+
+
+class FallbackImageLoader:
+    """Class created from group of Keras utilities, providing fallback
+    implementation to load images from filesystem into NumPy arrays
+    if no any images reading libraries installed.
+
+    This implementation is intentionally kept more simple then original one.
+    """
+
+    def __init__(self, channels_first=True):
+        self.pil_image = None
+        self.pil_interpolation = None
+        self.channels_first = channels_first
+        self._import_pil()
+
+    def _import_pil(self):
+        """Makes an attempt to load PIL library and setup available
+        interpolation methods.
+        """
+        try:
+            from PIL import ImageEnhance
+            from PIL import Image as pil_image
+        except ImportError:
+            raise ImportError(
+                'Fallback image loader requires PIL library to '
+                'convert image file into NumPy array.')
+        else:
+            self.pil_image = pil_image
+            self.pil_interpolation = pil_image.NEAREST
+
+    def load_image(self, path, target_size=None):
+        """Loads an image into PIL format.
+
+        Args:
+            path: Path to image file
+            target_size: Either `None` (default to original size)
+                or tuple of ints `(img_height, img_width)`.
+
+        Returns:
+            A numpy array.
+
+        Raises:
+            ImportError: If PIL is not available.
+            ValueError: If interpolation method is not supported.
+
+        """
+        img = self.pil_image.open(path)
+        if img.mode != 'RGB':
+            img = img.convert('RGB')
+        if target_size is not None:
+            width_height_tuple = (target_size[1], target_size[0])
+            if img.size != width_height_tuple:
+                img = img.resize(width_height_tuple, self.pil_interpolation)
+        return img
+
+    def image_to_array(self, img):
+        """Converts a PIL Image instance to a Numpy array.
+
+        Args:
+            img: PIL Image instance.
+
+        Returns:
+            A 3D Numpy array.
+
+        Raises:
+            ValueError: If invalid `img` or `data_format` is passed.
+
+        """
+        x = np.asarray(img, dtype=float)
+        if len(x.shape) == 3:
+            if self.channels_first:
+                x = x.transpose(2, 0, 1)
+        elif len(x.shape) == 2:
+            if self.channels_first:
+                x = x.reshape((1, x.shape[0], x.shape[1]))
+            else:
+                x = x.reshape((x.shape[0], x.shape[1], 1))
+        else:
+            raise ValueError('Unsupported image shape: ', x.shape)
+        return x
+
+    def __call__(self, filename, target_size=None):
+        pil_image = self.load_image(filename, target_size=target_size)
+        numpy_array = self.image_to_array(pil_image)
+        return numpy_array
