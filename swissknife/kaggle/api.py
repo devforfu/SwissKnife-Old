bf51f39857c4131008b47f3fbe868142c28b65b5
devforfu <developer.z@outlook.com>
2018-04-04 16:16:56 +0500

    Kaggle API submissions wrapper

--- swissknife/kaggle/api.py
+++ swissknife/kaggle/api.py
@@ -1,8 +1,19 @@
+import time
+import tempfile
+from io import StringIO
+from subprocess import Popen, PIPE
+
+import pandas as pd
+
+from ..config import console_logger
+
+
 class ClassifierSubmission:
     """Utility to make submissions for Kaggle's classification competitions."""
 
-    def __init__(self, floats_format='%1.17f'):
+    def __init__(self, floats_format='%1.17f', log=None):
         self.floats_format = floats_format
+        self.log = log or console_logger()
 
     def create(self, classes, predictions, output):
         """Creates a submission file.
@@ -36,3 +47,69 @@ class ClassifierSubmission:
                 'unexpected output type: %s.'
                 ' Only strings and file-like '
                 'objects are supported', type(output))
+
+    def submit(self, filename, competition, message=None, timeout=60):
+        if message is None:
+            message = filename
+
+        self.log.info('Sending submission...')
+        ok = self._send_submission(competition, message, filename)
+        if not ok:
+            self.log.error('Cannot submit results: submission command failed')
+            return None
+
+        self.log.info('Evaluating results...')
+        result = self._wait_for_evaluation(competition, timeout)
+        if not result:
+            self.log.error('Cannot retrieve submission result')
+            return None
+
+        return result
+
+    def _send_submission(self, competition, message, output):
+        cmd = 'kaggle competitions submit -c {} -m "{}" -f {}'
+        ok, _ = self._run_command(cmd.format(competition, message, output))
+        return ok
+
+    def _wait_for_evaluation(self, competition, timeout):
+        cmd = 'kaggle competitions submissions -c {} --csv'.format(competition)
+        start = time.time()
+        while True:
+            elapsed = time.time() - start
+            if elapsed >= timeout:
+                return None
+            ok, output = self._run_command(cmd, suppress=True)
+            if not ok:
+                return None
+            submission_csv = '\n'.join(output)
+            submissions = pd.read_csv(StringIO(submission_csv))
+            latest_submission = submissions.iloc[0]
+            if latest_submission.status != 'pending':
+                return latest_submission.to_dict()
+            time.sleep(1.0)
+
+    def _run_command(self, command, suppress=False):
+        process = Popen(
+            command, stdout=PIPE, stderr=PIPE,
+            bufsize=1, close_fds=True, shell=True)
+
+        command_output = []
+
+        while True:
+            if process.poll() is not None:
+                break
+            for pipe in (process.stdout, process.stderr):
+                while True:
+                    raw_output = pipe.readline()
+                    if not raw_output:
+                        break
+                    decoded = raw_output.decode(encoding='utf-8').strip('\n')
+                    if not decoded:
+                        continue
+                    command_output.append(decoded)
+                    if not suppress:
+                        self.log.info(decoded)
+
+        ok = process.returncode == 0
+        return ok, command_output
+
--- tests/kaggle/test_kaggle_submissions.py
+++ tests/kaggle/test_kaggle_submissions.py
@@ -3,16 +3,19 @@ from swissknife.tests import StringBuffer
 
 
 def test_classifier_submission_generates_valid_file():
+    """Tests generating submission CSV string in valid format from dictionary
+    with predictions and list with classes names.
+    """
     predictions = {
         'id1': [0.1, 0.6, 0.3],
         'id2': [0.05, 0.95, 0.0],
         'id3': [1.0, 0.0, 0.0]
     }
     classes = ['first', 'second', 'third']
-    submit = ClassifierSubmission(floats_format='%1.17f')
+    submission = ClassifierSubmission(floats_format='%1.17f')
 
     with StringBuffer() as buffer:
-        submit.create(classes, predictions, output=buffer)
+        submission.create(classes, predictions, output=buffer)
 
     string = buffer.captured
     assert string
@@ -21,12 +24,15 @@ def test_classifier_submission_generates_valid_file():
 
 
 def test_classifier_submission_users_specified_floats_formatting():
+    """Tests formatting floating point values representing submission scores
+    using provided formatting string.
+    """
     predictions = {'id1': [0.0001, 0.0005, 0.0005]}
     classes = ['first', 'second', 'third']
-    submit = ClassifierSubmission(floats_format='%1.3f')
+    submission = ClassifierSubmission(floats_format='%1.3f')
 
     with StringBuffer() as buffer:
-        submit.create(classes, predictions, output=buffer)
+        submission.create(classes, predictions, output=buffer)
 
     string = buffer.lines[-1]
     assert [float(x) == 0 for x in string.split(',')[1:]]
