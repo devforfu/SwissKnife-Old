1a3d9835ae3c0485f4656ff89a797cc471c3d18c
devforfu <developer.z@outlook.com>
2018-07-21 17:55:09 +0500

    New array iterator implementation

--- swissknife/iterators/arrays.py
+++ swissknife/iterators/arrays.py
@@ -1,3 +1,9 @@
+import math
+from itertools import cycle
+
+import numpy as np
+
+
 class BatchArrayIterator:
     """
 
@@ -9,4 +15,70 @@ class BatchArrayIterator:
                  infinite: bool=False,
                  same_size_batches: bool=False):
 
-        arrays = [array] + list(arrays)
+        if not infinite and same_size_batches:
+            raise ValueError('Incompatible configuration: cannot guarantee '
+                             'same size of batches when yielding finite '
+                             'number of files.')
+
+        arrays = _convert_to_arrays(array, *arrays)
+
+        self.arrays = arrays
+        self.batch_size = batch_size
+        self.infinite = infinite
+        self.same_size_batches = same_size_batches
+
+        self._n = _num_of_batches(arrays, batch_size, same_size_batches)
+        self._batch_index = 0
+        self._epoch_index = 0
+
+    @property
+    def n_batches(self) -> int:
+        """
+        Returns total number of batches required to iterate all elements of
+        the array.
+        """
+        return self._n
+
+    def __iter__(self):
+        self._batch_index = 0
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        if self._batch_index >= self._n:
+            if not self.infinite:
+                raise StopIteration()
+            self._batch_index = 0
+            self._epoch_index += 1
+
+        batches = tuple([self._take_next_batch(arr) for arr in self.arrays])
+        self._batch_index += 1
+        return batches[0] if len(batches) == 1 else batches
+
+    def _take_next_batch(self, array):
+        start = self._batch_index * self.batch_size
+        end = (self._batch_index + 1) * self.batch_size
+        return array[start:end]
+
+
+def _convert_to_arrays(seq, *seqs):
+    sequences = [seq] + list(seqs)
+    arrays = [np.asarray(seq) for seq in sequences]
+    n = len(arrays[0])
+    for arr in arrays[1:]:
+        if len(arr) != n:
+            raise ValueError('arrays should have the same length')
+    return arrays
+
+
+def _num_of_batches(arrays, batch_size, same_size):
+    n = len(arrays[0])
+    if same_size:
+        return n // batch_size
+    return int(math.ceil(n / batch_size))
+
+
+def _create_iterable(arrays, looped):
+    return cycle(arrays) if looped else iter(arrays)
---
+++ tests/iterators/test_batch_array_iteration.py
@@ -0,0 +1,90 @@
+from math import ceil
+
+import pytest
+import numpy as np
+
+from swissknife.iterators.arrays import BatchArrayIterator
+
+
+def test_batch_iterator_has_required_properties():
+    iterator = BatchArrayIterator(np.zeros(32))
+
+    assert not iterator.infinite
+    assert not iterator.same_size_batches
+    assert iterator.batch_size == 32
+
+
+@pytest.mark.parametrize('array_size,batch_size', [
+    (10, 1),
+    (128, 32),
+    (100, 32),
+    (1000, 10)
+])
+def test_finite_batch_iterator_yields_valid_number_of_batches(
+        array_size,
+        batch_size):
+
+    arr = np.zeros(array_size)
+    expected = ceil(array_size / batch_size)
+
+    iterator = BatchArrayIterator(arr, batch_size=batch_size)
+    batches = list(iterator)
+
+    assert len(batches) == expected
+    assert iterator.n_batches == expected
+
+
+@pytest.mark.parametrize('array_size,batch_size,last_size', [
+    (10, 3, 1),
+    (100, 15, 10),
+    (63, 32, 31)
+])
+def test_finite_batch_iterator_generates_non_even_batch_sizes(
+        array_size,
+        batch_size,
+        last_size):
+    """
+    Tests that the last batch yielded by the iterator has smaller size then
+    others if the length of iterated array is not divided by the batch size
+    parameter's value.
+    """
+    arr = np.zeros(array_size)
+
+    iterator = BatchArrayIterator(arr, batch_size=batch_size)
+    *_, last = list(iterator)
+
+    assert len(last) == last_size
+
+
+@pytest.mark.parametrize('array_size,batch_size', [
+    (10, 5),
+    (100, 25),
+    (256, 32)
+])
+def test_infinite_batch_iterator_indefinitely_yields_batches(
+        array_size,
+        batch_size):
+
+    arr = np.zeros(array_size)
+
+    iterator = BatchArrayIterator(
+        arr, batch_size=batch_size, infinite=True)
+    total = iterator.n_batches * 100
+    batches = [next(iterator) for _ in range(total)]
+
+    assert len(batches) == total
+
+
+def test_infinite_batch_iterator_yields_same_size_batches():
+    iterator = BatchArrayIterator(
+        np.zeros(10),
+        batch_size=8,
+        infinite=True,
+        same_size_batches=True)
+
+    b1 = next(iterator)
+    b2 = next(iterator)
+
+    assert iterator.n_batches == 1
+    assert len(b1) == len(b2)
+    assert np.array_equal(b1, b2)
